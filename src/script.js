13d7a69b5aca68be0df2d922e38867085b0c2a80
Mohamed Hamdy <99207823+Mohamed22221@users.noreply.github.com>
2024-03-04 17:02:03 +0200

    add position

--- src/script.js
+++ src/script.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
 
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 const canvas = document.querySelector("canvas.webgl");
 //add cube in scene
 const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.BoxBufferGeometry(1, 1, 1),
   new THREE.MeshBasicMaterial({ color: 0xff0000 })
 );
 
@@ -15,8 +15,8 @@ const sizes = {
 };
 
 //add camera in scene
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.set(0 , 0 , 2)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.set(0, 0, 2);
 
 scene.add(camera);
 
@@ -29,55 +29,9 @@ renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
 
-// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-
-// const scene = new THREE.Scene();
-
-// const canvas = document.querySelector("canvas.webgl");
-
-// const sizes = {
-//   width: window.innerWidth,
-//   height: window.innerHeight,
-// };
-
-// window.addEventListener("resize", () => {
-//   sizes.width = window.innerWidth;
-//   sizes.height = window.innerHeight;
-//   camera.aspect = sizes.width / sizes.height;
-//   camera.updateProjectionMatrix();
-//   renderer.setSize(sizes.width, sizes.height);
-//   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-// });
-
-// const cube = new THREE.Mesh(
-//   new THREE.BoxGeometry(4, 4, 4),
-//   new THREE.MeshBasicMaterial({ color: 0xff0000 })
-// );
-// scene.add(cube);
-// const camera = new THREE.PerspectiveCamera(
-//   75,
-//   sizes.width / sizes.height,
-//   0.1,
-//   100
-// );
-// camera.position.z = 20;
-// scene.add(camera);
-
-// const controls = new OrbitControls(camera, canvas);
-
-// const renderer = new THREE.WebGLRenderer({
-//   canvas,
-// });
-
-// renderer.setSize(sizes.width, sizes.height);
-// renderer.render(scene, camera);
-
-// const clock = new THREE.Clock();
-
-// const tick = () => {
-//   const elapsedTime = clock.getElapsedTime();
-
-//   renderer.render(scene, camera);
-//   window.requestAnimationFrame(tick);
-// };
-// tick();
+// show distance cube to camira
+console.log("distance to camira", cube.position.distanceTo(camera.position));
+// show length cube
+console.log("show length cube", cube.position.length());
+// normalize cube
+console.log("normalize cube", cube.position.normalize());
